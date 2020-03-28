b71a3a805b926665b1fabc9c31aa4122bb956f13
Thomas Haller <Thomas.Haller@lab10.coop>
2020-03-28 13:04:52 +0100

    added vc-js

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-class Student {
-  public fullName: string;
-  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
-  }
-}
 
-interface IPerson {
-  firstName: string;
-  lastName: string;
-}
+import * as vc from 'vc-js';
 
-function greeter(person: IPerson) {
-  return `Hello, ${person.firstName} ${person.lastName}`;
-}
 
-let user = new Student('Jane', 'M.', 'User');
-
-console.log(user);
+// async function test() {
+//   // Sample unsigned credential
+//   const credential = {
+//     "@context": [
+//       "https://www.w3.org/2018/credentials/v1",
+//       "https://www.w3.org/2018/credentials/examples/v1"
+//     ],
+//     "id": "https://example.com/credentials/1872",
+//     "type": ["VerifiableCredential", "AlumniCredential"],
+//     "issuer": "https://example.edu/issuers/565049",
+//     "issuanceDate": "2010-01-01T19:23:24Z",
+//     "credentialSubject": {
+//       "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+//       "alumniOf": "Example University"
+//     }
+//   };
+//   const signedVC = await vc.issue({credential, suite});
+//   console.log(JSON.stringify(signedVC, null, 2));
+// }
+ 
